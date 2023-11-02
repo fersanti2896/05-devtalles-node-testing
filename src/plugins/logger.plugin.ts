a6eee84c2ced6038c8e0d47c6db21f94de1edf92
Fernando Nicolás <fersanti2896@gmail.com>
2023-11-02 02:07:43 -0600

    Pruebas en logger.

--- src/plugins/logger.plugin.ts
+++ src/plugins/logger.plugin.ts
@@ -1,9 +1,7 @@
 import winston, { format } from 'winston';
-
 const { combine, timestamp, json } = format;
 
-
-const logger = winston.createLogger({
+export const logger = winston.createLogger({
   level: 'info',
   format: combine(
     timestamp(),
@@ -11,10 +9,6 @@ const logger = winston.createLogger({
   ),
   // defaultMeta: { service: 'user-service' },
   transports: [
-    //
-    // - Write all logs with importance level of `error` or less to `error.log`
-    // - Write all logs with importance level of `info` or less to `combined.log`
-    //
     new winston.transports.File({ filename: 'error.log', level: 'error' }),
     new winston.transports.File({ filename: 'combined.log' }),
   ],
@@ -25,7 +19,6 @@ logger.add(new winston.transports.Console({
 }));
 
 export const buildLogger = (service: string ) => {
-
   return {
     log: (message: string) => {
       logger.log('info', {message, service});
@@ -37,6 +30,4 @@ export const buildLogger = (service: string ) => {
       });
     }
   }
-
-
 }
